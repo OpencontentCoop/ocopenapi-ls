836173d2140adeebaee0340de555cd766db3e39c
Luca Realdi <luca.realdi@opencontent.it>
2025-02-06 00:46:18 +0100

    Fix api rrule

--- classes/OpenApi/SchemaFactory/ClassAttributePropertyFactories/EventFactoryProvider.php
+++ classes/OpenApi/SchemaFactory/ClassAttributePropertyFactories/EventFactoryProvider.php
@@ -13,15 +13,24 @@ class EventFactoryProvider extends ContentClassAttributePropertyFactory
             "type" => "string",
             "nullable" => true,
             "description" => $this->getPropertyDescription() . ' (iCalendar RFC pattern rule)',
-            "example" => "DTSTART=20200115T090000Z;DTEND=20200115T110000Z;FREQ=DAILY;INTERVAL=1;UNTIL=20200130T230000Z"
+            "example" => "FREQ=DAILY;UNTIL=20171215T235900;DTSTART=20171205T060000;DTEND=20171205T063000;INTERVAL=1",
         ];
     }
 
     public function serializeValue(Content $content, $locale)
     {
         $content = $this->getContent($content, $locale);
-        if (isset($content['input']['recurrencePattern'])){
-            return $content['input']['recurrencePattern'];
+        if (isset($content['input']['recurrencePattern'])) {
+            try {
+                if (!class_exists('\Recurr\Rule')) {
+                    throw new \Exception('Class "Recurr\Rule" not found');
+                }
+                $rrule = new \Recurr\Rule($content['input']['recurrencePattern']);
+                return $rrule->getString();
+            }catch (\Throwable $e){
+                \eZDebug::writeError($e->getMessage(), __METHOD__);
+                return $content['input']['recurrencePattern'];
+            }
         }
 
         return null;
