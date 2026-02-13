6036504550a7117787da92e21b7d6fe3479824b8
Luca Realdi <luca.realdi@opencontent.it>
2026-02-13 14:40:48 +0100

    Fix schemas module parameter

--- modules/schemas/schemas.php
+++ modules/schemas/schemas.php
@@ -2,12 +2,29 @@
 
 use Opencontent\OpenApi\SchemaFactory\ContentClassSchemaFactory;
 
+function camelToSnake($camelCase)
+{
+    $result = '';
+
+    for ($i = 0; $i < strlen($camelCase); $i++) {
+        $char = $camelCase[$i];
+
+        if (ctype_upper($char)) {
+            $result .= '_' . strtolower($char);
+        } else {
+            $result .= $char;
+        }
+    }
+
+    return ltrim($result, '_');
+}
+
 /** @var eZModule $Module */
 $Module = $Params['Module'];
 $Module->setExitStatus(eZModule::STATUS_IDLE);
 $http = eZHTTPTool::instance();
 $handler = $Params['Handler'];
-$classIdentifier = strtolower($Params['Identifier']);
+$classIdentifier = camelToSnake($Params['Identifier']);
 
 try {
     //@todo astrarre e gestire gli schema provider con un handler dedicato
@@ -18,7 +35,6 @@ try {
     $schemaFactory = new ContentClassSchemaFactory($classIdentifier);
     $schemaFactory->setSerializer(new RemoteIndexContentClassSchemaSerializer());
     $data = $schemaFactory->generateJsonSchema();
-
 } catch (Exception $e) {
     //@todo gestire il codice di stato in modo coerente all'errore
     header('HTTP/1.1 400 Bad Request');
